6ecafa8b6bdf69bdd54f881644272cf3c5dd2509
Ridwan <ridwan554@ymail.com>
2022-01-15 00:39:19 +0100

    more changes

---
+++ src/main/java/com/ridwan/api/clanewalletapi/ClaneWalletApiApplication.java
@@ -0,0 +1,13 @@
+package com.ridwan.api.clanewalletapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaneWalletApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClaneWalletApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ridwan/api/clanewalletapi/ClaneWalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ridwan.api.clanewalletapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaneWalletApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
